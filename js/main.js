79f98adfe587164bc9ecc0ec568ccb92f6539d59
Yaren Ahlatcı <yarenahlatc@gmail.com>
2023-08-04 14:48:45 +0300

    sass web site tüm dosyalar eklendi

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    let owl = $(".owl-carousel").owlCarousel({
+     autoplay: false,
+     dots:true,
+     dotsData:true,
+     loop:true,
+     nav:false,
+     items: 1
+    });
+    $('.owl-dot').click(function() {
+         owl.trigger('to.owl.carousel',[$(this).index(), 1000]);
+     })
+ });
+
+//  RESPONSIVE
+// OPEN - CLOSE menu açılması için
+
+$('#chek').change(function(){
+    if($(this).is(":checked")) {
+        $('.nav-header').addClass('active');
+    } else {
+        $('.nav-header').removeClass('active');
+    }
+});
